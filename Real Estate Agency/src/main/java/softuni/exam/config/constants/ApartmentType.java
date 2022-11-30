9b9ce1b345aa3dc4271c4902d2c1dd1808ef8d8b
Boris Velkov <95984793+bvelkov92@users.noreply.github.com>
2022-11-30 18:59:52 +0200

    Delete Real Estate Agency directory

--- Real Estate Agency/src/main/java/softuni/exam/config/constants/ApartmentType.java
+++
@@ -1,5 +0,0 @@
-package softuni.exam.config.constants;
-
-public enum ApartmentType {
-    two_rooms, three_rooms, four_rooms
-}
--- Real Estate Agency/src/main/java/softuni/exam/util/ValidationUtils.java
+++
@@ -1,8 +0,0 @@
-package softuni.exam.util;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface ValidationUtils {
-    <E> boolean isValid(E entity);
-}
